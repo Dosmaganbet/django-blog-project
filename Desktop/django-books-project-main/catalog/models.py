e525e10cf39d0399100c030319a52e1eecb0b70a
Doskhan <doskhandosm@gmail.com>
2026-02-03 19:48:02 +0500

    Final version for deployment

--- Desktop/django-books-project-main/catalog/models.py
+++ Desktop/django-books-project-main/catalog/models.py
@@ -9,13 +9,12 @@ class Author(models.Model):
 
 # Модель 2: Книга
 class Book(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Название книги")
-    # Связь с автором
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books', verbose_name="Автор")
-    published_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
-    
-    # ВОТ ЭТУ СТРОКУ МЫ ДОБАВИЛИ (Описание книги):
-    description = models.TextField(blank=True, verbose_name="Описание книги", help_text="Введите краткое содержание")
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    published_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+    # НОВАЯ СТРОКА:
+    external_url = models.URLField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.title
